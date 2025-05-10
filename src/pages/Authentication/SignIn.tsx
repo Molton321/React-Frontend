7d14588f431e3e2a3536160f373729e3b5338c1e
Juan-Carlos-Giraldo <jucar5454@gmail.com>
2025-05-10 17:34:26 -0500

    Handle redirect between signin and signup

--- src/pages/Authentication/SignIn.tsx
+++ src/pages/Authentication/SignIn.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import GoogleButton from '../../components/GoogleButton';
 import GreenButton from '../../components/GreenButton';
+import { useNavigate } from 'react-router-dom';
 
 const SignIn: React.FC = () => {
   const [isDesktop, setIsDesktop] = useState(window.innerWidth > 768);
@@ -20,26 +21,14 @@ const SignIn: React.FC = () => {
     console.log('Google Sign-In triggered');
   };
 
+  const navigate = useNavigate();
+
+  const handleSignUpRedirect = () => {
+    navigate('/auth/signup');
+  };
+
   return isDesktop ? (
     <div className="flex h-screen flex-col md:flex-row">
-      <div
-        className="hidden md:flex w-1/2 items-center justify-center relative"
-        style={{
-          backgroundImage: 'url(/src/images/others/food.jpg)',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          opacity: 1,
-        }}
-      >
-        <div className="absolute inset-0 bg-black opacity-30"></div>
-        <h1
-          className="relative text-white text-4xl md:text-6xl font-normal text-left px-1"
-          style={{ maxWidth: '400px' }}
-        >
-          Tu comida favorita está a un clic. ¡Inicia sesión y deja que te la
-          llevemos!
-        </h1>
-      </div>
       <div
         className="flex w-full h-full md:w-1/2 items-center justify-center sm:bg-white"
         style={{
@@ -58,8 +47,8 @@ const SignIn: React.FC = () => {
             <div className="flex flex-col items-center space-y-2">
               <GoogleButton onClick={handleGoogleSignIn}></GoogleButton>
               <GreenButton
-                onClick={handleGoogleSignIn}
-                text={'Ya tengo una cuenta'}
+                onClick={handleSignUpRedirect}
+                text={'No tengo una cuenta'}
               ></GreenButton>
             </div>
             <div className="flex items-start">
@@ -71,6 +60,24 @@ const SignIn: React.FC = () => {
             </div>
           </form>
         </div>
+      </div>
+      <div
+        className="hidden md:flex w-1/2 items-center justify-center relative"
+        style={{
+          backgroundImage: 'url(/src/images/others/food.jpg)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          opacity: 1,
+        }}
+      >
+        <div className="absolute inset-0 bg-black opacity-30"></div>
+        <h1
+          className="relative text-white text-4xl md:text-6xl font-normal text-left px-1"
+          style={{ maxWidth: '400px' }}
+        >
+          Tu comida favorita está a un clic. ¡Inicia sesión y deja que te la
+          llevemos!
+        </h1>
       </div>
     </div>
   ) : (
